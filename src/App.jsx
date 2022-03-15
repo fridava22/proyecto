f5840ad4f76f114600e2bd7a9a40500a11b85db9
frida valeria hernandez cabrera <isc18350715@gmail.com>
2022-03-15 16:21:17 -0600

    creacion de objetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-//ECMAscript 5
-var textoVar = "texto var 1";
-
-//ECMAscript 6
-let textoLet = "texto let 1";
-const textoConst = "texto const 1";
-
-textoVar = "texto var 2";
-textoLet = "texto let 2";
-// textoConst = "texto const 2";
-
-var textoVar = "texto var 3";
-// let textoLet = "texto let 3";
-// const textoConst = "texto const 3";
-
-if (true){
-var textoVar = 5;
-let textoLet = 10;
-//textoConst = 15;
-}
-
 const App = () => {
-return (
-<div className="App">
-<h1>{textoVar}</h1>
-<h1>{textoLet}</h1>
-<h1>{textoConst}</h1>
-</div>
-);
-}
+    const miObjeto = {
+        nombre: "frida valeria",
+        apPaterno: "hernandez",
+        apMaterno: "cabrera",
+        edad: "23"
+    };
+    const atributo = "apPaterno";
+
+    return (
+    <div className="App">
+      <h1>{`mi nombre es ${miObjeto.nombre} ${miObjeto.apPaterno} ${miObjeto.apMaterno} y tengo ${miObjeto.edad} años`}</h1>
+    </div>
+    );
+};
 
 export default App;
