449f8e3a1c59d64def8a4a3bdf8e26607307fae9
frida valeria hernandez cabrera <isc18350715@gmail.com>
2022-03-17 16:47:36 -0600

    practica con tablas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
 const App = () => {
-    const miObjeto = {
-        nombre: "frida valeria",
-        apPaterno: "hernandez",
-        apMaterno: "cabrera",
-        edad: "23"
-    };
-    const atributo = "apPaterno";
+  const persona = {
+  nombre: "Frida Valeria",
+  apellido: "Hernandez Cabrera",
+  edad: 23,
+  direccion:{
+  calle: "Emiliano Zapata",
+  numExt: 30,
+  numint: "",
+  ciudad: "San Lucas Ojitlan",
+  }
+  };
+  
+  const contactoPersona = {
+  telefono: "287 151 7439",
+  email: "isc18350715@gmail.com",
+  web: "www.thesimpsons.com",
+  };
+  
+  const infoPersona = {...persona,...contactoPersona, empresa: "Disney"};
+  infoPersona.apodo = "Bart";
+  
+  infoPersona.telefono = "555 555 5555";
+  
+  console.table(infoPersona);
+  
+  const{edad, nombre, apellido}= persona;
+  const{calle, numExt, numint,ciudad,} = persona.direccion;
+  const humanos = ["fabian", "liliana","fabian de jesus", "karol maximiliano"];
+  const[padre, madre, hermano1, hermano2]= humanos;
+  const mascotas = ["Santa's helper", "show ball"]
+  const familia =[...humanos,...mascotas, "Milhouse"]
+  console.log(familia) 
 
-    return (
-    <div className="App">
-      <h1>{`mi nombre es ${miObjeto.nombre} ${miObjeto.apPaterno} ${miObjeto.apMaterno} y tengo ${miObjeto.edad} años`}</h1>
-    </div>
-    );
-};
-
-export default App;
+  return (
+  <div className="App">
+  <h1>{`Mi nombre es ${nombre} ${apellido} y tengo ${edad} años y vivo en la C. ${calle} numero ${numExt},${ciudad}`}</h1>
+  <br />
+  {"Mi familia la conforman:"}
+  <ul>
+  <li>{`Mi padre: ${padre}`}</li>   
+  <li>{`Mi madre: ${madre}`}</li>
+  <li>{`Mis hermanos: ${hermano1} y ${hermano2}`}</li>
+  </ul>
+  </div>
+  );
+  }
+  
+  export default App;
